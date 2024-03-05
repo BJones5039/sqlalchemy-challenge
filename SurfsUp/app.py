7da4f504fc19fa9e6b4ee931e2afe8c83e5d6d21
BJones5039 <bjones5039@gmail.com>
2024-03-04 23:02:22 -0600

    updaes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,19 +1,29 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 
@@ -22,9 +32,59 @@
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+
+@app.route("/api/v1.0/stations")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    results = Session.query(station.station).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+
+@app.route("/api/v1.0/tobs")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    results = Session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).all()
+
+    Session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_tobs = []
+    for min, avg, max in results:
+        tobs_dict = {}
+        tobs_dict["min"] = min
+        tobs_dict["avg"] = avg
+        tobs_dict["max"] = max
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
